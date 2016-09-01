130019b66cd57d450ee3faddfe355215869122a3
Jeremie ges <ges.jeremie@gmail.com>
2016-09-01 03:31:02 +0300

    Design in progress

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,14 @@ var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   var app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      extension: 'scss',
+      includePaths: [
+        'bower_components/csstyle',
+        'bower_components/gridle/sass/gridle',
+        'bower_components/reset-css/'
+      ]
+    }
   });
 
   // Use `app.import` to add additional libraries to the generated
@@ -21,6 +28,8 @@ module.exports = function (defaults) {
   // along with the exports of each module as its value.
   app.import('bower_components/tabletop/src/tabletop.js');
   app.import('bower_components/markdown/lib/markdown.js');
+  app.import('bower_components/moment/min/moment.min.js');
+
 
   return app.toTree();
 };
