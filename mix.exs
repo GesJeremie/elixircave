4eeaf55176285966993ab664682b00063aad5c68
Jeremie ges <ges.jeremie@gmail.com>
2016-09-18 06:29:18 +0300

    Move folders

--- mix.exs
+++ mix.exs
@@ -51,6 +51,7 @@ defmodule Api.Mixfile do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-     "ecto.reset": ["ecto.drop", "ecto.setup"]]
+     "ecto.reset": ["ecto.drop", "ecto.setup"],
+     "deploy": ["edeliver update --branch=0.1.0-dev"]]
   end
 end
